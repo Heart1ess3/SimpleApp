8b4aa7f054ab05c97914faf69c713638b0c1700f
Heart1ess3 <tok220vtut@gmail.com>
2025-05-12 11:47:04 +0300

    create project

---
+++ SimpleApp.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using SimpleApp;
+
+namespace SimpleApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ SimpleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SimpleApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine($"3 + 5 = {calc.Add(3, 5)}");
+        }
+    }
+}
